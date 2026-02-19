bf125bea781ccbd49d3a99b9f488ed62b51a687a
Jude Dsouza <Jude_Dsouza@outlook.com>
2026-02-19 08:11:48 +0530

    Strings

---
+++ Chapter 3 - PS/04_problem4.py
@@ -0,0 +1,6 @@
+# Replace the double space from problem 3 with single space
+
+name = "Jude is a good  boy"
+
+print(name.replace("  ", " "))
+print(name) # Without the replace method, print(name) will print the original string
