488937606a680e87998984459efa51345a67f517
Jude Dsouza <Jude_Dsouza@outlook.com>
2026-02-20 19:58:38 +0530

    File I/O

---
+++ Chapter 9/02_file_write.py
@@ -0,0 +1,7 @@
+st = "Hey Jude you are amazing"
+
+f = open("myfile.txt", "w")
+
+f.write(st) # Writes the content written in st = "Hey Jude you are amazing"
+
+f.close()
---
+++ Chapter 9/04_append.py
@@ -0,0 +1,7 @@
+st = "Hey Jude you are amazing"
+
+f = open("myfile.txt", "a")
+
+f.write(st)
+
+f.close()
